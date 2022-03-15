bec3ee74cd02e55930b53b511115cfcee1daef39
RajPrasa <rajprasanna655@gmail.com>
2022-03-15 23:55:18 +0530

    version 1.0

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react"
+import ReactDOM from "react-dom"
+ReactDOM.render(<h1>React app</h1>,document.getElementById("root1"))
+ReactDOM.render(<h1>amazon app</h1>,document.getElementById("root2"))
